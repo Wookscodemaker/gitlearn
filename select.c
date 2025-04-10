3598ba3922ae9683a88dcb6266dfd56e2e2d1d8e
wooks luka <13028098282@163.com>
2025-04-10 12:45:47 +0800

    运行了一下

--- select.c
+++ select.c
@@ -3,7 +3,7 @@ void find(int *a, int n, int *maxsub, int *minsub);
 int main(void)
 {
     int maxsub, minsub, a[] = {5, 3, 7, 9, 2, 0, 4, 1, 6, 8};
-    find(&a, 10, &maxsub, &minsub); 
+    find(a, 10, &maxsub, &minsub); 
     printf("maxsub = %d, minsub = %d\n", maxsub, minsub);
     return 0;
 }
