6d826a9c82c86818ff1fbff4bb3fc1ad125cec8e
Wookscodemaker <116889381+Wookscodemaker@users.noreply.github.com>
2025-04-10 10:35:24 +0800

    创建 select.c

---
+++ select.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void find(int *a, int n, int *maxsub, int *minsub);
+int main(void)
+{
+    int maxsub, minsub, a[] = {5, 3, 7, 9, 2, 0, 4, 1, 6, 8};
+    find(&a, 10, &maxsub, &minsub); 
+    printf("maxsub = %d, minsub = %d\n", maxsub, minsub);
+    return 0;
+}
+void find(int *a, int n, int *maxsub, int *minsub)
+{
+    int i;
+    *maxsub = 0;
+    *minsub = 0;
+    for(i = 1; i < n; i++)
+    {
+        if(a[i] > a[*maxsub]) *maxsub = a[i];
+        if(a[i] < a[*minsub]) *minsub = a[i];
+    }
+}
